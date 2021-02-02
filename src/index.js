cd6d14f6c240a08737234f8eb8bd269dbc3bbc66
ACoolCat <36032266+ACoolCat@users.noreply.github.com>
2021-02-02 10:58:27 -0500

    total calculated

--- src/index.js
+++ src/index.js
@@ -1,3 +1,36 @@
 export function calculateTotal (items, tax) {
-  // TODO
+  if (-1 < tax > 1) {
+    throw error("Tax must be between 0 and 1");
+  }
+  let holder = 0;
+  for (var i = 0; i < items.length; i++) {
+    switch(items[i].taxable){
+      case false:
+        holder = items[i].price + holder;
+        break;
+      case true:
+        holder = ((items[i].price * Math.abs(tax)) + items[i].price) + holder;
+        break;
+    }
+  }
+  return holder;
 }
+
+// Example items array
+const items = [
+  {
+    price: 100,
+    taxable: false
+  },
+  {
+    price: 50,
+    taxable: false
+  },
+  {
+    price: 250,
+    taxable: true
+  }
+]
+
+// Example call to calculateTotal
+calculateTotal(items, .0975) // 100 + 50 + 250 + (250 * .0975) = 424.375
